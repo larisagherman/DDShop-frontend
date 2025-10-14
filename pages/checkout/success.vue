920f3e102ad0504a1c916065ae138a8909bb9432
Gherman Larisa <gherman.larisa3@gmail.com>
2025-10-14 08:20:59 +0300

    height

--- pages/checkout/success.vue
+++ pages/checkout/success.vue
@@ -10,7 +10,7 @@ onMounted(() => {
 })
 </script>
 <template>
-  <div class="h-screen p-20">
+  <div class="mb-30 p-20">
     <div class="flex flex-col justify-center items-center gap-2">
       <h1 class="text-4xl ">Thank you!</h1>
       <p class="text-2xl mb-2">Your treats are in the oven.</p>
